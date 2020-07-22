83bc1741e7d79f0ee71e6508367a959f4f8d5700
ZxcSoft <hu18650@163.com>
2020-07-22 04:18:28 +0000

    Deploying to master from  @ 93f7a36aaf2f6db4d8791451da37c34215beb663 🚀

--- custom.js
+++ custom.js
@@ -1,6 +1,33 @@
-document.onreadystatechange = function(){
-    if(document.readyState == "complete"){
-        var a = document.querySelector("#app > div.global-ui > div.reco-bgm-panel > div.reco-bgm-box > div.reco-bgm-info > div.reco-bgm-operation > i.reco-bgm.reco-bgm-play.play");
-        a && a.style.display !== "none" && a.click();
-    }
+var a_idx = 0;
+
+function getRandom(max, min) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
+jQuery(document).ready(function ($) {
+    $("body").click(function (e) {
+        var a = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正", "法治", "爱国", "敬业", "诚信", "友善");
+        var $i = $("<span/>").text(a[a_idx]);
+        a_idx = (a_idx + 1) % a.length;
+        var x = e.pageX,
+            y = e.pageY;
+        $i.css({
+            "z-index": 99999999999999999999999,
+            "top": y - 20,
+            "left": x,
+            "position": "absolute",
+            "font-weight": "bold",
+            "color": `rgb(${getRandom(255,0)},${getRandom(255,0)},${getRandom(255,0)})`,
+            "user-select": 'none',
+            "cursor": 'default'
+        });
+        $("body").append($i);
+        $i.animate({
+                "top": y - 180,
+                "opacity": 0
+            },
+            1500,
+            function () {
+                $i.remove();
+            });
+    });
+});
